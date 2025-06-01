eada826e824eeb1468c04ab12c2becffc3ef3e36
✿ corey (they/them) <c@rey.foo>
2025-06-01 10:17:07 -0700

    Formatting

--- flake.nix
+++ flake.nix
@@ -5,27 +5,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        nixpkgsArgs = { inherit system; config = { }; };
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        nixpkgsArgs = {
+          inherit system;
+          config = {};
+        };
         nixpkgs-23-05 = import inputs.nixpkgs-23-05 nixpkgsArgs;
         freckle = inputs.freckle.packages.${system};
-      in
-      rec {
+      in rec {
         packages = rec {
           nodejs = freckle.nodejs-18-18-x;
           typescript = nixpkgs-23-05.typescript;
         };
-        devShells.js =
-          nixpkgs-23-05.mkShell {
-            name = "js";
-            buildInputs =
-              with packages;
-              [
-                nodejs
-                typescript
-              ];
-          };
+        devShells.js = nixpkgs-23-05.mkShell {
+          name = "js";
+          buildInputs = with packages; [
+            nodejs
+            typescript
+          ];
+        };
         devShells.default = devShells.js;
         formatter = nixpkgs-23-05.alejandra;
       }
